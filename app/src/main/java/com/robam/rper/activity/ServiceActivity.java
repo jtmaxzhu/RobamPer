3f5058726b0425a0c81bbef466bdf5d6b62b2211
jtmaxzhu <30967723+jtmaxzhu@users.noreply.github.com>
2019-11-18 21:33:18 +0800

    编写服务demo

--- app/src/main/java/com/robam/rper/activity/ServiceActivity.java
+++ app/src/main/java/com/robam/rper/activity/ServiceActivity.java
@@ -1,8 +1,18 @@
 package com.robam.rper.activity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +23,13 @@ import com.robam.rper.R;
 import com.robam.rper.annotation.EntryActivity;
 import com.robam.rper.serviceTest.MyService;
 
+import static com.liulishuo.filedownloader.util.DownloadServiceNotConnectedHelper.startForeground;
+
 @EntryActivity(icon = R.drawable.xn, name = "服务工具", index = 3)
 public class ServiceActivity extends BaseActivity implements View.OnClickListener {
 
     public static final int UPDATE_TEXT = 1;
+
     private TextView textView;
     private Handler handler = new Handler(){
         public void handleMessage(Message msg){
@@ -29,6 +42,21 @@ public class ServiceActivity extends BaseActivity implements View.OnClickListene
         }
     };
 
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder)iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +65,14 @@ public class ServiceActivity extends BaseActivity implements View.OnClickListene
         Button changText = findViewById(R.id.change_text);
         Button startService = findViewById(R.id.start_service);
         Button stopService = findViewById(R.id.stop_service);
+        Button bind_service = findViewById(R.id.bind_service);
+        Button unbind_service = findViewById(R.id.unbind_service);
         changText.setOnClickListener(this);
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
+        unbind_service.setOnClickListener(this);
+        bind_service.setOnClickListener(this);
+
 
     }
 
@@ -57,7 +90,21 @@ public class ServiceActivity extends BaseActivity implements View.OnClickListene
                 }).start();
                 break;
             case R.id.start_service:
-                Intent intent = new Intent(this, MyService.class);
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.unbind_service:
+                Intent unbindIntent = new Intent(this, MyService.class);
+                unbindService(connection);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);//绑定服务
+                break;
             default:
                 break;
         }
--- app/src/main/java/com/robam/rper/serviceTest/MyService.java
+++ app/src/main/java/com/robam/rper/serviceTest/MyService.java
@@ -1,16 +1,83 @@
 package com.robam.rper.serviceTest;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.robam.rper.R;
+import com.robam.rper.activity.ServiceActivity;
+import com.robam.rper.util.LogUtil;
+
+import static com.liulishuo.filedownloader.util.DownloadServiceNotConnectedHelper.startForeground;
 
 public class MyService extends Service {
+    private static final String TAG = "MyService";
+    private static Notification notification;
+    private DownloadBinder mBinder = new DownloadBinder();
+    public class DownloadBinder extends Binder{
+        public void startDownload(){
+            LogUtil.d(TAG,"startDownload executed");
+        }
+        public int getProgress(){
+            LogUtil.d(TAG,"getProgress executed");
+            return 0;
+        }
+    }
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+       return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogUtil.d(TAG,"onCreate executed");
+        Intent intent = new Intent(this, ServiceActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0 ,intent,0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel("channel_002", "name2", NotificationManager.IMPORTANCE_LOW);
+            NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(mChannel);
+            notification = new Notification.Builder(this)
+                    .setChannelId("channel_002")
+                    .setContentTitle("通知")
+                    .setContentText("服务以及启动")
+                    .setContentIntent(pi)
+                    .setSmallIcon(R.mipmap.ic_launcher).build();
+        }else{
+            notification = new NotificationCompat.Builder(this)
+                    .setContentTitle("这是通知标题")
+                    .setContentText("这是通知内容")
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                    .setContentIntent(pi).build();
+        }
+
+        startForeground(1, notification);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        LogUtil.d(TAG,"onDestroy executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        LogUtil.d(TAG,"onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
     }
 }
